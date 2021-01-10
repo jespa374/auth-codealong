71a2cd9ce03eb408316d25919e87357da40e0267
JESSICA PANDITHA <JESSICA.PANDITHA@GMAIL.COM>
2021-01-10 15:44:06 +0100

    finished codealong

--- server.js
+++ server.js
@@ -1,24 +1,91 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt-nodejs';
 
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+});
+
+//This is a middleware that checks if the accesstoken finds a user that matches 
+//the accessToken stored in the DB and the one in the header
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header('Authorization')});
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(401).json({ loggedOut: true });
+  }
+}
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+  res.send('Hello world');
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    //Here the user is registered with name, email and password"
+    const { name, email, password } = req.body;
+    // DO NOT STORE PLAINTEXT PASSWORDS
+    const user = new User({ name, email, password: bcrypt.hashSync(password)});
+    user.save();
+    //We get an accessToken when the user logs in
+    res.status(201).json({ id: user._id, accessToken: user.accessToken });
+  } catch (err) {
+    res.status(400).json({ message: 'Could not create user', errors: err.errors });
+  }
+});
+
+//Here we are protecting the secrets endpoint. If authenticateUser is true then you have access to the secret
+app.get('/secrets', authenticateUser);
+app.get('/secrets', (req, res) => {
+  res.json({ secret: 'This is a super secret message' });
+});
+
+//Here we are allowing the user to log in
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({email: req.body.email});
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({userId: user._id, accessToken: user.accessToken});
+  } else {
+    res.json({notFound: true});
+  }
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
